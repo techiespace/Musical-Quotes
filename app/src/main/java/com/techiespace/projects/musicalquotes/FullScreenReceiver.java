3e3832425dbeca5927d1da2e2304ea366243661f
techiespace <shreyas.techiespace@gmail.com>
2017-05-20 08:23:18 +0530

    Added FullScreen Activity

--- app/src/main/java/com/techiespace/projects/musicalquotes/FullScreenReceiver.java
+++ app/src/main/java/com/techiespace/projects/musicalquotes/FullScreenReceiver.java
@@ -10,7 +10,7 @@ public class FullScreenReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Inside Receiver", Toast.LENGTH_SHORT).show();
-        Intent i1 = new Intent(context, MainActivity.class);    //MainActivity here to be later replaced by FullScreenActivity
+        Intent i1 = new Intent(context, FullscreenLyricsActivity.class);    //MainActivity here to be later replaced by FullScreenActivity
         i1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i1);
     }
